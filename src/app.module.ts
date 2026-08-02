20072815240f3eb9627887bd4ed9e3b41066d736
rauf105 <abdurraufrx@gmail.com>
2026-08-02 23:45:03 +0600

    app module update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerModule } from './customer/customer.module';
-//import { AdminModule } from './admin/admin.module';
+import { AdminModule } from './admin/admin.module';
 import { SellerModule } from './seller/seller.module';
 
 @Module({
@@ -13,13 +13,13 @@ import { SellerModule } from './seller/seller.module';
       host: process.env.DB_HOST || 'localhost',
       port: Number(process.env.DB_PORT) || 5432,
       username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || 'root',
+      password: process.env.DB_PASS || 'postgres',
       database: process.env.DB_NAME || 'online_marketplace',
       autoLoadEntities: true,
       synchronize: true, // dev only — generates tables automatically via TypeORM
     }),
     CustomerModule, // Member 1
-    // AdminModule, // Member 2
+    AdminModule, // Member 2
     SellerModule, // Member 3
   ],
 })
